d1ec61593b520da71d301d1bafda9014c697ef3b
alan <langwdalan@gmail.com>
2024-08-22 21:54:23 +0800

    feat: Update birthday summary text

--- src/calendar.rs
+++ src/calendar.rs
@@ -1,4 +1,4 @@
-use chinese_lunisolar_calendar::{LunisolarDate, SolarDate};
+use chinese_lunisolar_calendar::{LunarDay, LunarMonth, LunisolarDate, SolarDate};
 use chrono::{Datelike, NaiveDate, Utc};
 use icalendar::{Calendar, Component, Event, EventLike};
 
@@ -9,11 +9,12 @@ pub fn create_calendar(config: config::BirthdayConfig) -> Calendar {
   config.birthdays.iter().for_each(|birthday| {
     let date = create_solar_birthday(birthday.clone());
     match date {
-      Ok(date) => {
+      Ok(result) => {
+        let (date, month, day) = result;
         let event = Event::new()
           .summary(&format!(
-            "🎂 {}的生日 (农历{}月{})",
-            birthday.name, birthday.month, birthday.day
+            "🎂 {}的生日-农历{:#}{:#}",
+            birthday.name, month, day
           ))
           .all_day(date)
           .done();
@@ -27,7 +28,9 @@ pub fn create_calendar(config: config::BirthdayConfig) -> Calendar {
   calendar
 }
 
-fn create_solar_birthday(birthday: config::Birthday) -> anyhow::Result<NaiveDate> {
+fn create_solar_birthday(
+  birthday: config::Birthday,
+) -> anyhow::Result<(NaiveDate, LunarMonth, LunarDay)> {
   let current_year = Utc::now().year() as u16;
   let lunar =
     LunisolarDate::from_ymd(current_year, birthday.month, false, birthday.day).map_err(|err| {
@@ -38,6 +41,8 @@ fn create_solar_birthday(birthday: config::Birthday) -> anyhow::Result<NaiveDate
       )
     })?;
   let gregorian = SolarDate::from(lunar);
+  let lunar_month = lunar.to_lunar_month();
+  let lunar_day = lunar.to_lunar_day();
   let date = gregorian.to_naive_date();
-  Ok(date)
+  Ok((date, lunar_month, lunar_day))
 }
