cbe859a53a9352f981b7eccb6d5c08e3473fbfb2
alan <langwdalan@gmail.com>
2025-01-03 09:49:45 +0800

    ✨ expand calendar to three years

--- src/calendar.rs
+++ src/calendar.rs
@@ -5,35 +5,43 @@ use icalendar::{Calendar, Component, Event, EventLike};
 use crate::config;
 
 pub fn create_calendar(config: config::BirthdayConfig) -> Calendar {
+  // 生成 去年 今年 明年 三个年份的 vec 数据
+  let years = vec![
+    Utc::now().year() as u16 - 1,
+    Utc::now().year() as u16,
+    Utc::now().year() as u16 + 1,
+  ];
   let mut calendar = Calendar::new();
-  config.birthdays.iter().for_each(|birthday| {
-    let date = create_solar_birthday(birthday.clone());
-    match date {
-      Ok(result) => {
-        let (date, month, day) = result;
-        let event = Event::new()
-          .summary(&format!(
-            "🎂 {}的生日-农历{:#}{:#}",
-            birthday.name, month, day
-          ))
-          .all_day(date)
-          .done();
-        calendar.push(event);
+  years.iter().for_each(|year| {
+    config.birthdays.iter().for_each(|birthday| {
+      let date = create_solar_birthday(birthday.clone(), *year);
+      match date {
+        Ok(result) => {
+          let (date, month, day) = result;
+          let event = Event::new()
+            .summary(&format!(
+              "🎂 {}的生日-农历{:#}{:#}",
+              birthday.name, month, day
+            ))
+            .all_day(date)
+            .done();
+          calendar.push(event);
+        }
+        Err(err) => {
+          log::error!("{}", err);
+        }
       }
-      Err(err) => {
-        log::error!("{}", err);
-      }
-    }
+    });
   });
   calendar
 }
 
 fn create_solar_birthday(
   birthday: config::Birthday,
+  year: u16,
 ) -> anyhow::Result<(NaiveDate, LunarMonth, LunarDay)> {
-  let current_year = Utc::now().year() as u16;
   let lunar =
-    LunisolarDate::from_ymd(current_year, birthday.month, false, birthday.day).map_err(|err| {
+    LunisolarDate::from_ymd(year, birthday.month, false, birthday.day).map_err(|err| {
       anyhow::anyhow!(
         "Failed to create lunar date for birthday '{}': {}",
         birthday.name,
